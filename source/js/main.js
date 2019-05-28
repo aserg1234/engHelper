dc665fc141e453b8ccdace1087491aa734bd8f9a
Antonina Padlovna <antoninaSobaka@example.com>
2019-05-29 01:22:03 +0300

    function addCategory, delCategory, storeReview were added

--- source/js/main.js
+++ source/js/main.js
@@ -15,6 +15,11 @@
    var inpStore = document.getElementsByName('inpStore');
    var btnCreateNewStore = document.querySelector('.btn_createNewStore');
    var btnDeleteStore = document.querySelector('.btn_deleteStore');
+   var btnAddNewCat = document.querySelector('.btn_addNewCat');  
+   var btnDelNewCat = document.querySelector('.btn_delNewCat');     
+   var newCategory = document.getElementsByName('newCategory');
+   var uniqueCategory = document.getElementsByName('uniqueCategory');
+   var btnPreview = document.querySelector('.btn_preview');     
    var pathToDbTheme = document.getElementsByName('pathDbTheme');
    var dataNewForDb = document.getElementsByName('newData');
    var dataOldFromDb = document.getElementsByName('oldData');
@@ -33,9 +38,10 @@
    const DB_STORE_OF_STORES = {};
    var dbStoreName = '';
    var db;
+   var newCategoryObj = {};
  
    var current_view_pub_key;
- 
+   //создание базы данных и базового хранилища 
    function openDb() {
      console.log("openDb ...");
      var req = indexedDB.open(DB_NAME);//, DB_VERSION
@@ -75,17 +81,16 @@
       };
       DB_VERSION = db.version;
       req.onupgradeneeded = function (evt) {
-      DB_VERSION += 1;
-      console.log("openDb.onupgradeneeded");
-       var thisDB = evt.target.result;
-       //if (!thisDB.objectStoreNames.contains(inpStore[0].value)) {
-          var store = thisDB.createObjectStore(
-            inpStore[0].value, { keyPath: 'id', autoIncrement: true });
-            
-          store.createIndex('biblioid', 'biblioid', { unique: true });
-          store.createIndex('title', 'title', { unique: false });
-          store.createIndex('year', 'year', { unique: false });
-        //}
+        DB_VERSION += 1;
+        console.log("openDb.onupgradeneeded");
+        var thisDB = evt.target.result;
+        //if (!thisDB.objectStoreNames.contains(inpStore[0].value)) {
+        var store = thisDB.createObjectStore(
+          inpStore[0].value, { keyPath: 'id', autoIncrement: true });
+
+        for(var i in newCategoryObj){
+          store.createIndex( i, i, { unique: newCategoryObj[i] });
+        }
 
       };
     }
@@ -100,72 +105,100 @@
     };
  
     DB_VERSION = db.version;
-alert(DB_VERSION); 
     req.onupgradeneeded = function (evt) {
 
       DB_VERSION += 1;
       console.log("openDb.onupgradeneeded");
        var thisDB = evt.target.result;
        thisDB.deleteObjectStore(inpStore[0].value);
-alert(DB_VERSION); 
       };
 
-  }    
+  }   
+  
+  //добавить категорию(поле)
+  function addCategory(){
+    newCategoryObj[newCategory[0].value] = uniqueCategory[0].value;//
+  }
 
-    //обработчики событий
-    function addEventListeners(){
-     
-      //обработка создания нового хранилища
-      btnCreateNewStore.addEventListener("click", createNewStore);
-      //обработка удаления хранилища
-      btnDeleteStore.addEventListener("click", deleteStore);
-
-
-
+  //удалить категорию(поле)
+  function delCategory(){
+    for(var i in newCategoryObj){
+      if(i == newCategory[0].value){
+        delete newCategoryObj[i];
+      }
     }
+  }  
+  //предпросмотр добавляемых категорий хранилища
+  function storePreview(){
+    var str = '';
+    for(var  i in newCategoryObj){
+      str += "категория: " + i + " = уникальнось: " + newCategoryObj[i] + " \n";
+    }  
+    alert(str);  
+  }
+
+  //обработчики событий
+  function addEventListeners(){
+    
+    //обработка создания нового хранилища
+    btnCreateNewStore.addEventListener("click", createNewStore);
+
+    //обработка удаления хранилища
+    btnDeleteStore.addEventListener("click", deleteStore);
+
+    //обработка добавление новой категории
+    btnAddNewCat.addEventListener("click", addCategory);
+
+    //обработка удаление новой категории
+    btnDelNewCat.addEventListener("click", delCategory);  
+    
+    //обработка предпросмотр структуры хранилища
+    btnPreview.addEventListener("click", storePreview);        
+
+  }
 
    //////////////////////////////////////////////////////
    //кнопки
-   var btnNewDate = document.querySelector('.btn_new_date');
-   btnNewDate.addEventListener('click', sendNewDataInDB);
+  //  var btnNewDate = document.querySelector('.btn_new_date');
+  //  btnNewDate.addEventListener('click', sendNewDataInDB);
 
-   var btnOldDate = document.querySelector('.btn_old_date');
-   btnOldDate.addEventListener('click', sendChangeDataInDB);
+  //  var btnOldDate = document.querySelector('.btn_old_date');
+  //  btnOldDate.addEventListener('click', sendChangeDataInDB);
 
-   var btnExerciseDate = document.querySelector('.btn_exercise_date');
-   btnExerciseDate.addEventListener('click', getRandExercise);
+  //  var btnExerciseDate = document.querySelector('.btn_exercise_date');
+  //  btnExerciseDate.addEventListener('click', getRandExercise);
 
-   var btnThemeDate = document.querySelector('.btn_theme_date');
-   btnThemeDate.addEventListener('click', getThemeExercise);
+  //  var btnThemeDate = document.querySelector('.btn_theme_date');
+  //  btnThemeDate.addEventListener('click', getThemeExercise);
 
-   var btnSubmitDecision = document.querySelector('.btn_submit_decision');
-   btnSubmitDecision.addEventListener('click', sendDecision);
+  //  var btnSubmitDecision = document.querySelector('.btn_submit_decision');
+  //  btnSubmitDecision.addEventListener('click', sendDecision);
 
 
-   /////////////////////////////////////////////////
+  //  /////////////////////////////////////////////////
  
-   function sendNewDataInDB(){
+  //  function sendNewDataInDB(){
 
-      // alert('sendNewDataInDB');
-   }
+  //     // alert('sendNewDataInDB');
+  //  }
 
-   function sendChangeDataInDB(){
+  //  function sendChangeDataInDB(){
 
-   }
+  //  }
 
-   function getRandExercise(){
+  //  function getRandExercise(){
 
-   }
+  //  }
 
-   function getThemeExercise(){
+  //  function getThemeExercise(){
 
-   }
+  //  }
 
-   function sendDecision(){
+  //  function sendDecision(){
 
-   }
+  //  }
 
 
    openDb();
    addEventListeners();
-})(); // Immediately-Invoked Function Expression (IIFE)
+})();
