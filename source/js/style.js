bbbeae259ec043d4de21db8172fa6ceeb2da3121
sergei <antoninaSobaka@example.com>
2019-06-27 15:52:14 +0300

    serch menu was added.

--- source/js/style.js
+++ source/js/style.js
@@ -1,10 +1,11 @@
 (function(){
 
     var btnCloseCreateDB = document.querySelector('.btn_close_create_db');
-    var btnCloseUseDB = document.querySelector('.btn_close_use_db');    
+    var btnCloseUseDB = document.querySelector('.btn_close_use_db');   
+    var btnCloseSearchInStore = document.querySelector('.btn_close_search_in_store');        
     var btnOpenCreateDbMenu = document.querySelector('.btn_open_create_db');
     var btnOpenChangeDbMenu = document.querySelector('.btn_open_change_db');
-
+    var btnOpenSearchInSoreMenu = document.querySelector('.btn_open_search_in_db');
     // function openNav(){
     //     var openedElement;
     //     if(this.className.contains('btn_open_create_db')){
@@ -27,10 +28,14 @@
         document.querySelector('.overlay_use_db').style.height = "100%";
 
     }
+    function openSearchInStore(){
+        document.querySelector('.overlay_search_in_store').style.height = "100%";
 
+    }
     function closeOverlay(){
         document.querySelector('.overlay_create_db').style.height = "0";
         document.querySelector('.overlay_use_db').style.height = "0";
+        document.querySelector('.overlay_search_in_store').style.height = "0";        
     }
 
     function addEventListeners(){
@@ -39,14 +44,23 @@
         btnCloseCreateDB.addEventListener('click', closeOverlay);
 
         //закрыть меню использования БД
-        btnCloseUseDB.addEventListener('click', closeOverlay);        
+        btnCloseUseDB.addEventListener('click', closeOverlay);   
+        
+        //закрыть меню поиска в хранилище
+        btnCloseSearchInStore.addEventListener('click', closeOverlay);          
         
         //открыть меню создания БД
         btnOpenCreateDbMenu.addEventListener('click', openCreateDb);
 
         //открыть меню изменения БД
         btnOpenChangeDbMenu.addEventListener('click', openUseDb);
+
+        //открыть меню поиск в хранилище
+        btnOpenSearchInSoreMenu.addEventListener('click', openSearchInStore);
+        
     }
+
+
 
     addEventListeners();
 })();
